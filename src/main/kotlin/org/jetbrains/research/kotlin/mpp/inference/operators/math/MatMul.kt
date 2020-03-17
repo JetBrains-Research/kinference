13b706779d4095a0269f6a2223c1207464d606c6
Anastasia Tuchina <anastasia.i.tuchina@gmail.com>
2020-03-17 23:53:04 +0300

    Minor refactoring

--- src/main/kotlin/org/jetbrains/research/kotlin/mpp/inference/operators/math/MatMul.kt
+++ src/main/kotlin/org/jetbrains/research/kotlin/mpp/inference/operators/math/MatMul.kt
@@ -6,10 +6,6 @@ import org.jetbrains.research.kotlin.mpp.inference.tensors.Tensor
 class MatMul<T : Number> : Operator<T>() {
     override fun apply(inputs: Collection<Tensor<T>>): Collection<Tensor<T>> {
         require(inputs.size == 2) { "Applicable only for two arguments" }
-
-        val left = inputs.first()
-        val right = inputs.last()
-
-        return listOf(left dot right)
+        return listOf(inputs.first() dot inputs.last())
     }
 }
