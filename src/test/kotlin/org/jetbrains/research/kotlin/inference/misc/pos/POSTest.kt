30f6048cb8d44230784957b61b10268d2137ca6b
Pavel Bakhvalov <pavelkrot@gmail.com>
2020-08-06 21:47:21 +0300

    Remove main from tests

--- src/test/kotlin/org/jetbrains/research/kotlin/inference/misc/pos/POSTest.kt
+++ src/test/kotlin/org/jetbrains/research/kotlin/inference/misc/pos/POSTest.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.research.kotlin.inference.misc.pos
 
 import org.jetbrains.research.kotlin.inference.Utils
+import org.jetbrains.research.kotlin.inference.model.Model
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class POSTest {
     private fun getTargetPath(dirName: String) = "/pos/$dirName/"
@@ -9,5 +12,31 @@ class POSTest {
     @Test
     fun test_pos_tagger() {
         Utils.tensorTestRunner(getTargetPath("test_pos_tagger"))
+    }
+
+    @Test
+    @Disabled
+    fun test_performance_for_pos_model() {
+        val path = object {}.javaClass.getResource("/pos/test_pos_tagger/").path
+        val model = Model.load(path + "model.onnx")
+        val dataSet = File(path).list()!!.filter { "test" in it }.map {
+            val inputFiles = File("$path/$it").walk().filter { file -> "input" in file.name }
+
+            val inputTensors = inputFiles.map { Utils.getTensor(it) }.toList()
+            inputTensors
+        }.first()
+
+        val count = 100
+        val times = LongArray(count)
+        for (i in (0 until count)) {
+            val startTime = System.currentTimeMillis()
+            model.predict(dataSet)
+            val endTime = System.currentTimeMillis()
+            times[i] = endTime - startTime
+        }
+
+        val time = times.average()
+        println(time)
+        assert(time < 300f)
     }
 }
