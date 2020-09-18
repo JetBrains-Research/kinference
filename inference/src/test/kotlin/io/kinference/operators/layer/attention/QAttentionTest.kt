4bbc57c67d382862fbb84f3afc68163b723b4f64
Anastasia Tuchina <anastasia.tuchina@jetbrains.com>
2020-09-18 14:34:50 +0300

    Add new QAttention test

--- inference/src/test/kotlin/io/kinference/operators/layer/attention/QAttentionTest.kt
+++ inference/src/test/kotlin/io/kinference/operators/layer/attention/QAttentionTest.kt
@@ -10,4 +10,9 @@ class QAttentionTest {
     fun `test quantized attention defaults`() {
         Utils.tensorTestRunner(getTargetPath("test_qattention_defaults"))
     }
+
+    @Test
+    fun `test quantized attention`() {
+        Utils.tensorTestRunner(getTargetPath("test_qattention_op"))
+    }
 }
---
+++ inference/src/test/resources/qattention/test_qattention_op/test_data_set_0/input_3.pb
@@ -0,0 +1,1 @@
+Binput_scaleJ5w-<
