f84f9e6778b081d4e4d55489d470bccfd24cc23d
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2020-10-01 16:38:26 +0300

    Generation info

---
+++ inference/src/test/kotlin/io/kinference/generating/GenerationInfoTest.kt
@@ -0,0 +1,31 @@
+package io.kinference.generating
+
+import org.junit.jupiter.api.Test
+
+class GenerationInfoTest {
+    @Test
+    fun testEmpty() {
+        val gi = GenerationInfo()
+        assert(gi.probs.size == 0)
+    }
+
+    @Test
+    fun testAdd() {
+        val probs = listOf(0.1, 0.4)
+        val gi = GenerationInfo(probs)
+        assert(gi.probs.size == 2)
+        gi.add(0.3)
+        assert(gi.probs.size == 3)
+        assert(gi.probs[2] == 0.3)
+    }
+
+    @Test
+    fun testTrim() {
+        val probs = listOf(0.1, 0.4, 0.3)
+        var gi = GenerationInfo(probs)
+        assert(gi.probs.size == 3)
+        gi = gi.trim(1, 2)
+        assert(gi.probs.size == 1)
+        assert(gi.probs[0] == 0.4)
+    }
+}
