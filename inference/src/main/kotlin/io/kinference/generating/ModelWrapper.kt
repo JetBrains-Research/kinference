c3bcf6e1db429a5489da11eca989f2d0f9552781
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2020-10-05 17:40:06 +0300

    Next steps of model and test on it

--- inference/src/main/kotlin/io/kinference/generating/ModelWrapper.kt
+++ inference/src/main/kotlin/io/kinference/generating/ModelWrapper.kt
@@ -3,22 +3,26 @@ package io.kinference.generating
 import io.kinference.data.tensors.Tensor
 import io.kinference.data.tensors.asTensor
 import io.kinference.model.Model
-import io.kinference.ndarray.FloatNDArray
-import io.kinference.ndarray.LongNDArray
-import io.kinference.ndarray.NDArray
-import io.kinference.ndarray.Strides
+import io.kinference.ndarray.*
 
 interface ModelWrapper {
-    fun initLogProbs(inputIds: List<Int>): Pair<List<List<MutableList<Double>>>, List<NDArray>>
+    fun initLogProbs(inputIds: List<List<Int>>): Pair<List<List<MutableList<Double>>>, List<MutableNDArray>>
 
-    fun initLastLogProbs(inputIds: List<Int>): Pair<List<MutableList<Double>>, List<NDArray>> {
+    fun initLastLogProbs(inputIds: List<List<Int>>): Pair<List<MutableList<Double>>, List<MutableNDArray>> {
         val (score, mems) = initLogProbs(inputIds)
         // (batchSize, seqLen, vocabSize) -> (batchSize, vocabSize)
         val lastProbs: List<MutableList<Double>> = score.map { seq -> seq[seq.size - 1] }
         return Pair(lastProbs, mems)
     }
 
-    fun getLogProbs(inputIds: List<Int>, past: List<NDArray>): Pair<List<MutableList<Double>>, List<NDArray>>
+    fun getLogProbs(inputIds: List<List<Int>>, past: List<MutableNDArray>): Pair<List<List<MutableList<Double>>>, List<MutableNDArray>>
+
+    fun getLastLogProbs(inputIds: List<Int>, past: List<MutableNDArray>): Pair<List<MutableList<Double>>, List<MutableNDArray>> {
+        val (score, mems) = getLogProbs(listOf(inputIds), past)
+        // (batchSize, seqLen, vocabSize) -> (batchSize, vocabSize)
+        val lastProbs: List<MutableList<Double>> = score.map { seq -> seq[seq.size - 1] }
+        return Pair(lastProbs, mems)
+    }
 }
 
 class OnnxModelWrapper(modelPath: String) : ModelWrapper {
@@ -30,20 +34,50 @@ class OnnxModelWrapper(modelPath: String) : ModelWrapper {
 //    distilgpt2_l3_h12_d256_int8
 
     @ExperimentalUnsignedTypes
-    override fun initLogProbs(inputIds: List<Int>): Pair<List<List<MutableList<Double>>>, List<NDArray>> {
-        val seqLen = inputIds.size
-        val input = ArrayList<Tensor>()
-        input.add(LongNDArray(inputIds.map { it.toLong() }.toLongArray(), Strides(intArrayOf(1, seqLen))).asTensor("input_ids"))
-        input.add(FloatNDArray(List(seqLen) { 1.0F }.toFloatArray(), Strides(intArrayOf(1, seqLen))).asTensor("attention_mask"))
-        input.add(LongNDArray(inputIds.indices.map { it.toLong() }.toLongArray(), Strides(intArrayOf(1, seqLen))).asTensor("position_ids"))
+    override fun initLogProbs(inputIds: List<List<Int>>): Pair<List<List<MutableList<Double>>>, List<MutableNDArray>> {
+        val batchSize = inputIds.size
+        if (batchSize == 0) {
+            return Pair(emptyList(), emptyList())
+        }
 
-        val batchSize = 1
+        val seqLen = inputIds[0].size
+        val input = ArrayList<Tensor>()
+        input.add(LongNDArray(inputIds.flatten().map { it.toLong() }.toLongArray(), Strides(intArrayOf(batchSize, seqLen))).asTensor("input_ids"))
+        input.add(FloatNDArray(List(batchSize * seqLen) { 1.0F }.toFloatArray(), Strides(intArrayOf(batchSize, seqLen))).asTensor("attention_mask"))
+        input.add(LongNDArray(inputIds.flatten().indices.map { it.toLong() }.toLongArray(), Strides(intArrayOf(batchSize, seqLen))).asTensor("position_ids"))
+
         val shape = intArrayOf(2, batchSize, numAttentionHeads, 0, hiddenSize / numAttentionHeads)
         for (i in 0 until numLayer) {
             val emptyPast = FloatNDArray(floatArrayOf(0.0f), Strides(shape))
             input.add(emptyPast.asTensor("past_$i"))
         }
 
+        return process(input, batchSize, seqLen)
+    }
+
+    @ExperimentalUnsignedTypes
+    override fun getLogProbs(inputIds: List<List<Int>>, past: List<MutableNDArray>): Pair<List<List<MutableList<Double>>>, List<MutableNDArray>> {
+        val batchSize = inputIds.size
+        if (batchSize == 0) {
+            return Pair(emptyList(), emptyList())
+        }
+
+        val seqLen = inputIds[0].size
+        val pastLength = past[0].shape[3]
+
+        val input = ArrayList<Tensor>()
+        input.add(LongNDArray(inputIds.flatten().map { it.toLong() }.toLongArray(), Strides(intArrayOf(batchSize, seqLen))).asTensor("input_ids"))
+        input.add(FloatNDArray(List(batchSize * (pastLength + seqLen)) { 1.0F }.toFloatArray(), Strides(intArrayOf(batchSize, pastLength + seqLen))).asTensor("attention_mask"))
+        val positions = inputIds.map { (pastLength until pastLength + seqLen).map { it.toLong() }.toList() }
+        input.add(LongNDArray(positions.flatten().toLongArray(), Strides(intArrayOf(batchSize, seqLen))).asTensor("position_ids"))
+
+        past.forEachIndexed { i, state -> input.add(state.asTensor("past_$i")) }
+        // (2, 1, 4, 4, 64)
+
+        return process(input, batchSize, seqLen)
+    }
+
+    private fun process(input: ArrayList<Tensor>, batchSize: Int, seqLen: Int): Pair<List<List<MutableList<Double>>>, List<MutableNDArray>> {
         val output = model.predict(input)
         val ndProbs = (output[0] as Tensor).data
 
@@ -56,10 +90,6 @@ class OnnxModelWrapper(modelPath: String) : ModelWrapper {
             }
         }
 
-        return Pair(probs, output.subList(1, output.size).map { (it as Tensor).data })
-    }
-
-    override fun getLogProbs(inputIds: List<Int>, past: List<NDArray>): Pair<List<MutableList<Double>>, List<NDArray>> {
-        TODO("Not yet implemented")
+        return Pair(probs, output.subList(1, output.size).map { (it as Tensor).data.toMutable() })
     }
 }
