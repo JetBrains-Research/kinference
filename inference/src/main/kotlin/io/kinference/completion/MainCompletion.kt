af35c18f14f70b0e89df0a65a3e5065aea55da20
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2020-10-13 22:07:24 +0300

    Test speed

--- inference/src/main/kotlin/io/kinference/completion/MainCompletion.kt
+++ inference/src/main/kotlin/io/kinference/completion/MainCompletion.kt
@@ -1,6 +1,7 @@
 package io.kinference.completion
 
 import io.kinference.completion.suggest.*
+import java.lang.System.currentTimeMillis
 
 class MainCompletion {
     companion object {
@@ -15,6 +16,11 @@ class MainCompletion {
 
             val completionModel = CompletionModel(completionsCollector, rankingModel, filterModel, postFilterModel)
 
+//            interaction(completionModel)
+            speedTest(completionModel, 30, 50)
+        }
+
+        private fun interaction(completionModel: CompletionModel) {
             println("Write something")
             var input = "hello wo"
             while (true) {
@@ -29,6 +35,33 @@ class MainCompletion {
 
                 input = readLine().toString()
             }
+        }
+
+        private fun speedTest(completionModel: CompletionModel, len: Int = 1, itersNum: Int = 100) {
+//            1 - 0.97258
+            println("Warm up")
+            val input = (0 until len).map { "hello " }.joinToString { "" } + "wo"
+//            val input = "hello wo"
+            for (i in 0 until 10) {
+                val sepIndex = input.lastIndexOf(' ')
+                val context = input.substring(0, sepIndex)
+                val prefix = input.substring(sepIndex)
+
+                val completions = completionModel.complete(context, prefix, config)
+            }
+
+            println("Start")
+            val startTime = currentTimeMillis()
+            for (i in 0 until itersNum) {
+                val sepIndex = input.lastIndexOf(' ')
+                val context = input.substring(0, sepIndex)
+                val prefix = input.substring(sepIndex)
+
+                val completions = completionModel.complete(context, prefix, config)
+            }
+            val duration = (currentTimeMillis() - startTime) / 1000.0 / itersNum
+            println()
+            println(duration)
         }
     }
 }
