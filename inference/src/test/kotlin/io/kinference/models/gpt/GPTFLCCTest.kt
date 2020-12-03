53f11d6e6820238da96a378a09809fa952b38195
Vladislav.Tankov <vladislav.tankov@jetbrains.com>
2020-12-03 16:25:34 +0300

    Disable for now tests of FLCC model

--- inference/src/test/kotlin/io/kinference/models/gpt/GPTFLCCTest.kt
+++ inference/src/test/kotlin/io/kinference/models/gpt/GPTFLCCTest.kt
@@ -2,16 +2,17 @@ package io.kinference.models.gpt
 
 import io.kinference.runners.PerformanceRunner
 import io.kinference.runners.TestRunner
-import org.junit.jupiter.api.Tag
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 
 class GPTFLCCTest {
+    @Disabled
     @Test
     @Tag("heavy")
     fun `test gpt py model`() {
         TestRunner.runFromS3("/gpt2/flcc-py-completion/quantized/v1/", "tests/gpt2/flcc-py-completion/quantized/v1", delta = 0.7)
     }
 
+    @Disabled
     @Test
     @Tag("heavy")
     fun `test gpt py performance`() {
