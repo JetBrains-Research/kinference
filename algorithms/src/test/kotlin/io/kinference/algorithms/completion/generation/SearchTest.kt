27b957450bd7e4ace28fbaebabbfa9cb6ff50bbc
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2020-12-10 15:56:48 +0300

    Fix search test and mark quality measure as not test

--- algorithms/src/test/kotlin/io/kinference/algorithms/completion/generation/SearchTest.kt
+++ algorithms/src/test/kotlin/io/kinference/algorithms/completion/generation/SearchTest.kt
@@ -17,13 +17,13 @@ class SearchTest {
         val stepLogProbs = arrayOf(doubleArrayOf(0.13, 0.4, 0.17, 0.1, 0.2))
         val sortMask = search.step(stepLogProbs, context)
 
-        assertArrayEquals(sortMask, intArrayOf(0, 0, 0, 0))
-        val targetScores1 = doubleArrayOf(0.4000, 0.2000, 0.1700, 0.1000)
+        assertArrayEquals(intArrayOf(0, 0, 0, 0, 0), sortMask)
+        val targetScores1 = doubleArrayOf(0.40, 0.20, 0.17, 0.13, 0.10)
         assertTrue(search.scores.mapIndexed { i, d -> abs(d - targetScores1[i]) }.all { it < 1e-5 })
 
         val sortMask2 = search.step(Array(4) { doubleArrayOf(0.21, 0.11, 0.18, 0.14, 0.36) }, context)
-        assertArrayEquals(sortMask2, intArrayOf(0, 0, 1, 0, 2))
-        val targetScores2 = doubleArrayOf(0.76, 0.58, 0.56, 0.54, 0.53)
+        assertArrayEquals(intArrayOf(0, 0, 0, 1, 0), sortMask2)
+        val targetScores2 = doubleArrayOf(0.76, 0.61, 0.58, 0.56, 0.54)
         assertTrue(search.scores.mapIndexed { i, d -> abs(d - targetScores2[i]) }.all { it < 1e-5 })
     }
 }
--- algorithms/src/test/kotlin/io/kinference/algorithms/completion/model/QualityTest.kt
+++ algorithms/src/test/kotlin/io/kinference/algorithms/completion/model/QualityTest.kt
@@ -7,41 +7,47 @@ import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
 class QualityTest {
-    companion object {
-        private val text = """
-            Kotless stands for Kotlin serverless framework.
+    private val text = """
+        Kotless stands for Kotlin serverless framework.
 
-            Its focus lies in reducing the routine of serverless deployment creation by generating it straight from the code of the application itself.
-            
-            So, simply speaking, Kotless gives you one magic button to deploy your Web application as a serverless application on AWS!
-            
-            Kotless consists of two main parts:
-            
-            DSL provides a way of defining serverless applications. There are three DSLs supported:
-            Kotless DSL — Kotless own DSL that provides annotations to declare routing, scheduled events, etc.
-            Ktor — Ktor engine that is introspected by Kotless. Use standard Ktor syntax and Kotless will generate deployment.
-            Spring Boot — Spring Boot serverless container that is introspected by Kotless. Use standard Spring syntax and Kotless will generate deployment.
-            Kotless Gradle Plugin provides a way of deploying serverless application. For that, it:
-            performs the tasks of generating Terraform code from the application code and, subsequently, deploying it to AWS;
-            runs application locally, emulates the AWS environment and provides the possibility for in-IDE debugging.
-            One of the key features of Kotless is its ability to embed into existing applications. Kotless makes super easy deployment of existing Spring and Ktor applications to AWS serverless platform.
-        """.trimIndent()
+        Its focus lies in reducing the routine of serverless deployment creation by generating it straight from the code of the application itself.
+        
+        So, simply speaking, Kotless gives you one magic button to deploy your Web application as a serverless application on AWS!
+        
+        Kotless consists of two main parts:
+        
+        DSL provides a way of defining serverless applications. There are three DSLs supported:
+        Kotless DSL — Kotless own DSL that provides annotations to declare routing, scheduled events, etc.
+        Ktor — Ktor engine that is introspected by Kotless. Use standard Ktor syntax and Kotless will generate deployment.
+        Spring Boot — Spring Boot serverless container that is introspected by Kotless. Use standard Spring syntax and Kotless will generate deployment.
+        Kotless Gradle Plugin provides a way of deploying serverless application. For that, it:
+        performs the tasks of generating Terraform code from the application code and, subsequently, deploying it to AWS;
+        runs application locally, emulates the AWS environment and provides the possibility for in-IDE debugging.
+        One of the key features of Kotless is its ability to embed into existing applications. Kotless makes super easy deployment of existing Spring and Ktor applications to AWS serverless platform.
+    """.trimIndent()
 
-        private const val prefix = " m"
-    }
-
-
-    @Test
-    @Tag("heavy")
-    fun `test golf`() {
+//    @Test
+//    @Tag("heavy")
+    fun main() {
         val completionModel = CompletionModelFactory.createCompletionModel(CompletionModels.v6)
         val metric = GolfMetric()
-        val golfScore = metric.compute(completionModel, text, context_len = 50, CompletionModels.v6)
+        val start = System.currentTimeMillis()
+        val golfScore = metric.compute(completionModel, text, contextLen = 256, CompletionModels.v6)
 
         println(golfScore)  // 0.6530454895913647
-        assert(golfScore < 0.9)
-//        ĊĊ
+        println("Time: " + (System.currentTimeMillis() - start))
+        assert(golfScore < 0.8)
 
-//        0.6507324595219738 - после фильтрации неправильных токенов
+        //        0.6507324595219738 - после фильтрации неправильных токенов
+//        0.7987663839629915 - repPen 5
+//        0.7833461835003855 - repPen 2
+
+//        бан
+//        0.6507324595219738
+//        Time: 380461
+
+//        без бана
+//        0.6507324595219738
+//        Time: 365310
     }
 }
