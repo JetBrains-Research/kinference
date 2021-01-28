dd71a4220981d51993bbccbc2fc483211b925694
Vladislav Tankov <Vladislav.Tankov@jetbrains.com>
2021-01-28 18:39:23 +0300

    Remove GEC from KInference since it was moved to internal repository

--- algorithms/src/main/kotlin/io/kinference/algorithms/gec/tokenizer/Tokenizer.kt
+++
@@ -1,9 +0,0 @@
-package io.kinference.algorithms.gec.tokenizer
-
-/**
- * Basic interface for tokenization of text to words or sentences
- */
-interface Tokenizer {
-    /** Performs tokenization of text */
-    fun tokenize(text: String): List<String>
-}
--- algorithms/src/main/kotlin/io/kinference/algorithms/gec/utils/Utils.kt
+++
@@ -1,4 +0,0 @@
-package io.kinference.algorithms.gec.utils
-
-fun IntRange.withOffset(offset: Int) = IntRange(this.start + offset, this.endInclusive + offset)
-fun IntRange.withRightExpand(expand: Int) = IntRange(this.start, this.endInclusive + expand)
