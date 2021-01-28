fc96d08b9d1ef1c1c008e3ce854b5f14488fff47
Vladislav Tankov <Vladislav.Tankov@jetbrains.com>
2021-01-28 15:50:41 +0300

    Fix GenerationInfoTest -- add ids

--- algorithms/src/test/kotlin/io/kinference/algorithms/completion/generation/GenerationInfoTest.kt
+++ algorithms/src/test/kotlin/io/kinference/algorithms/completion/generation/GenerationInfoTest.kt
@@ -12,7 +12,8 @@ class GenerationInfoTest {
     @Test
     fun testAdd() {
         val probs = listOf(0.1, 0.4)
-        val gi = GenerationInfo(probs)
+        val ids = listOf(1, 2, 3)
+        val gi = GenerationInfo(probs, ids)
         assert(gi.probs.size == 2)
         gi.add(1, 0.3)
         assert(gi.probs.size == 3)
@@ -22,7 +23,8 @@ class GenerationInfoTest {
     @Test
     fun testTrim() {
         val probs = listOf(0.1, 0.4, 0.3)
-        var gi = GenerationInfo(probs)
+        val ids = listOf(1, 2, 3)
+        var gi = GenerationInfo(probs, ids)
         assert(gi.probs.size == 3)
         gi = gi.trim(1, 2)
         assert(gi.probs.size == 1)
