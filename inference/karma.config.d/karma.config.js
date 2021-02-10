372b900b6477cf148b5fc232a23e68cbf5d2929c
Vladislav Tankov <Vladislav.Tankov@jetbrains.com>
2021-02-11 01:07:27 +0300

    Finally make work JS tests

--- inference/karma.config.d/karma.config.js
+++ inference/karma.config.d/karma.config.js
@@ -25,6 +25,9 @@ config.plugins.push({
 });
 
 config.set({
+  browserDisconnectTimeout: 60000,
+  browserNoActivityTimeout: 60000,
+  pingTimeout: 60000,
   client: {
     mocha: {
       timeout: 60000
--- ndarray/karma.config.d/karma.config.js
+++ ndarray/karma.config.d/karma.config.js
@@ -1,4 +1,7 @@
 config.set({
+  browserDisconnectTimeout: 60000,
+  browserNoActivityTimeout: 60000,
+  pingTimeout: 60000,
   client: {
     mocha: {
       timeout: 60000
