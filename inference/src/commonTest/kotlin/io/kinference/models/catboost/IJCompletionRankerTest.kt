2ca0a7c8de7170ff452ce9d5aedbdedd2f58fd62
Vladislav Tankov <Vladislav.Tankov@jetbrains.com>
2021-02-11 00:57:50 +0300

    Commit forgotten files

---
+++ inference/src/commonTest/kotlin/io/kinference/models/catboost/IJCompletionRankerTest.kt
@@ -0,0 +1,18 @@
+package io.kinference.models.catboost
+
+import io.kinference.runners.AccuracyRunner
+import io.kinference.runners.PerformanceRunner
+import io.kinference.utils.TestRunner
+import kotlin.test.Test
+
+class IJCompletionRankerTest {
+    @Test
+    fun test_ranker() = TestRunner.runTest {
+        AccuracyRunner.runFromS3("catboost:ij-completion-ranker:v1")
+    }
+
+    @Test
+    fun test_ranker_performance() = TestRunner.runTest {
+        PerformanceRunner.runFromS3("catboost:ij-completion-ranker:v1", count = 5)
+    }
+}
