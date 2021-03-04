7506ee219e4fadddfcb81ce592b8dba35e1ec126
Anastasia Tuchina <anastasia.tuchina@jetbrains.com>
2021-03-04 19:55:11 +0300

    Publish task for serializer module

--- serialization/build.gradle.kts
+++ serialization/build.gradle.kts
@@ -1,3 +1,5 @@
+import tanvd.kosogor.proxy.publishJar
+
 group = rootProject.group
 version = rootProject.version
 
@@ -9,4 +11,17 @@ dependencies {
     api(kotlin("stdlib"))
     api(project(":ndarray"))
     api("com.squareup.wire", "wire-runtime", "3.6.0")
+}
+
+publishJar {
+    bintray {
+        username = "tanvd"
+        repository = "io.kinference"
+        info {
+            description = "KInference model serializer module"
+            vcsUrl = "https://github.com/JetBrains-Research/kinference"
+            githubRepo = "https://github.com/JetBrains-Research/kinference"
+            labels.addAll(listOf("kotlin", "inference", "ml", "serialization"))
+        }
+    }
 }
