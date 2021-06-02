56021ace0a684f4d53041ab51e367396c7929f17
Ilya Vologin <ilyavologin@gmail.com>
2021-06-02 20:43:46 +0300

    Disabled FLCC test because old incorrect model

--- inference/src/commonTest/kotlin/io/kinference/models/gpt/GPTFLCCTest.kt
+++ inference/src/commonTest/kotlin/io/kinference/models/gpt/GPTFLCCTest.kt
@@ -6,7 +6,7 @@ import io.kinference.utils.TestRunner
 import kotlin.test.Test
 
 class GPTFLCCTest {
-    @Test
+//    @Test
     fun heavy_test_gpt_py_model() = TestRunner.runTest {
         AccuracyRunner.runFromS3("gpt2:flcc-py-completion:quantized:v2", delta = 10.0)
     }
