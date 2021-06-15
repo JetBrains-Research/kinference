e69839c21b2772ae194fa5f683118b9d241d36a2
Ilya Vologin <ilyavologin@gmail.com>
2021-06-15 17:37:45 +0300

    Re-enabling GEC tests with minimized dataset

--- inference/src/commonTest/kotlin/io/kinference/models/bert/GECTest.kt
+++ inference/src/commonTest/kotlin/io/kinference/models/bert/GECTest.kt
@@ -8,9 +8,16 @@ import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
 class GECTest {
-    //@Test
+    @Test
     fun heavy_test_gec_model() = TestRunner.runTest {
-        AccuracyRunner.runFromS3("bert:gec:en:standard:v2")
+        AccuracyRunner.runFromS3("bert:gec:en:standard:v2", disableTests = listOf(
+            "test_data_set_batch_32_seqLen_32",
+            "test_data_set_batch_32_seqLen_64",
+            "test_data_set_batch_32_seqLen_92",
+            "test_data_set_batch_32_seqLen_128",
+            "test_data_set_batch_32_seqLen_256",
+            "test_data_set_batch_32_seqLen_512",
+        ))
     }
 
     @Test
--- inference/src/commonTest/kotlin/io/kinference/runners/AccuracyRunner.kt
+++ inference/src/commonTest/kotlin/io/kinference/runners/AccuracyRunner.kt
@@ -18,39 +18,43 @@ object AccuracyRunner {
 
     data class ONNXTestData(val name: String, val actual: List<ONNXData>, val expected: List<ONNXData>)
 
-    private suspend fun runTestsFromS3(name: String): List<ONNXTestData> {
+    private suspend fun runTestsFromS3(name: String, disableTests: List<String> = emptyList()): List<ONNXTestData> {
         val toFolder = name.replace(":", "/")
-        return runTestsFromFolder(S3TestDataLoader, toFolder)
+        return runTestsFromFolder(S3TestDataLoader, toFolder, disableTests)
     }
 
-    private suspend fun runTestsFromResources(testPath: String): List<ONNXTestData> {
+    private suspend fun runTestsFromResources(testPath: String, disableTests: List<String> = emptyList()): List<ONNXTestData> {
         val path = "build/processedResources/${TestRunner.forPlatform("js", "jvm")}/test/${testPath}"
-        return runTestsFromFolder(ResourcesTestDataLoader, path)
+        return runTestsFromFolder(ResourcesTestDataLoader, path, disableTests)
     }
 
-    private suspend fun runTestsFromFolder(loader: TestDataLoader, path: String): List<ONNXTestData> {
+    private suspend fun runTestsFromFolder(loader: TestDataLoader, path: String, disableTests: List<String> = emptyList()): List<ONNXTestData> {
         val model = Model.load(loader.bytes(TestDataLoader.Path(path, "model.onnx")))
         val files = loader.text(TestDataLoader.Path(path, "descriptor.txt")).lines()
         return files.filter { "test" in it }.groupBy { file -> file.takeWhile { it != '/' } }.map { (group, files) ->
-            val inputFiles = files.filter { file -> "input" in file }
-            val inputTensorProtos = inputFiles.map { TensorProto.decode(loader.bytes(TestDataLoader.Path(path, it))) }
-            val inputTensors = inputTensorProtos.map{ model.graph.prepareInput(it) }
+            if (group in disableTests) {
+                null
+            } else {
+                val inputFiles = files.filter { file -> "input" in file }
+                val inputTensorProtos = inputFiles.map { TensorProto.decode(loader.bytes(TestDataLoader.Path(path, it))) }
+                val inputTensors = inputTensorProtos.map{ model.graph.prepareInput(it) }
 
-            val outputFiles =  files.filter { file -> "output" in file }
-            val expectedOutputTensors = outputFiles.map { DataLoader.getTensor(loader.bytes(TestDataLoader.Path(path, it))) }.toList()
+                val outputFiles =  files.filter { file -> "output" in file }
+                val expectedOutputTensors = outputFiles.map { DataLoader.getTensor(loader.bytes(TestDataLoader.Path(path, it))) }.toList()
 
-            logger.info { "Start predicting: $group" }
-            val actualOutputTensors = model.predict(inputTensors)
-            ONNXTestData(group, expectedOutputTensors, actualOutputTensors)
-        }
+                logger.info { "Start predicting: $group" }
+                val actualOutputTensors = model.predict(inputTensors)
+                ONNXTestData(group, expectedOutputTensors, actualOutputTensors)
+            }
+        }.filterNotNull()
     }
 
-    suspend fun runFromS3(name: String, delta: Double = AccuracyRunner.delta) {
-        check(runTestsFromS3(name), delta)
+    suspend fun runFromS3(name: String, delta: Double = AccuracyRunner.delta, disableTests: List<String> = emptyList()) {
+        check(runTestsFromS3(name, disableTests), delta)
     }
 
-    suspend fun runFromResources(path: String, delta: Double = AccuracyRunner.delta) {
-        check(runTestsFromResources(path), delta)
+    suspend fun runFromResources(path: String, delta: Double = AccuracyRunner.delta, disableTests: List<String> = emptyList()) {
+        check(runTestsFromResources(path, disableTests), delta)
     }
 
     private fun check(datasets: List<ONNXTestData>, delta: Double = AccuracyRunner.delta) {
