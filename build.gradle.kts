28717a97c37cc175c9a64b28fc6cff8b18082acb
Ilya Vologin <ilyavologin@gmail.com>
2021-08-02 15:07:38 +0300

    Update version for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 group = "io.kinference"
-version = "0.1.3"
+version = "0.1.4"
 
 plugins {
     kotlin("multiplatform") version "1.4.30" apply false
