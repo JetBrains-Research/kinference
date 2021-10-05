cde5e44e7a4b249bd70ea333fd83c6eba40ed900
Ilya Vologin <ilyavologin@gmail.com>
2021-10-05 16:02:52 +0300

    Disable large tests in GEC for CI

--- tfjs-inference/src/jsTest/kotlin/io/kinference/models/bert/GECTest.kt
+++ tfjs-inference/src/jsTest/kotlin/io/kinference/models/bert/GECTest.kt
@@ -8,7 +8,14 @@ import kotlin.test.Test
 class GECTest {
     @Test
     fun heavy_test_gec_model() = TestRunner.runTest {
-        AccuracyRunner.runFromS3("bert:gec:en:standard:v2")
+        AccuracyRunner.runFromS3("bert:gec:en:standard:v2", disableTests = listOf(
+            "test_data_set_batch_32_seqLen_32",
+            "test_data_set_batch_32_seqLen_64",
+            "test_data_set_batch_32_seqLen_92",
+            "test_data_set_batch_32_seqLen_128",
+            "test_data_set_batch_32_seqLen_256",
+            "test_data_set_batch_32_seqLen_512",
+        ))
     }
 
     @Test
