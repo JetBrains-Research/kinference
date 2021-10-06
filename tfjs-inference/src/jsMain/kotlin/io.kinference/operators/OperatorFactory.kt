ddcd08365c6bb6dbea7e4160086348ea1b180c6f
Ilya Vologin <ilyavologin@gmail.com>
2021-10-06 15:00:56 +0300

    Unsqueeze operator implemented

--- tfjs-inference/src/jsMain/kotlin/io.kinference/operators/OperatorFactory.kt
+++ tfjs-inference/src/jsMain/kotlin/io.kinference/operators/OperatorFactory.kt
@@ -30,6 +30,7 @@ object OperatorFactory {
         "LayerNormalization" -> LayerNormalization(attributes, inputs, outputs)
         "QAttention" -> QAttention(attributes, inputs, outputs)
         "Gather" -> Gather(attributes, inputs, outputs)
+        "Unsqueeze" -> Unsqueeze(attributes, inputs, outputs)
         else -> error("Unsupported operator: $opType")
     } as Operator<ONNXData, ONNXData>
 
