e0f9917a9bb877650d6b96d7c4911625d68d8ef5
Ilya Vologin <ilya.vologin@jetbrains.com>
2021-12-06 19:30:16 +0300

    Update quant delta for GPTR model

--- inference/inference-core/src/commonTest/kotlin/io/kinference/models/gpt/GPTRTest.kt
+++ inference/inference-core/src/commonTest/kotlin/io/kinference/models/gpt/GPTRTest.kt
@@ -2,6 +2,7 @@ package io.kinference.models.gpt
 
 import io.kinference.KITestEngine.KIAccuracyRunner
 import io.kinference.KITestEngine.KIPerformanceRunner
+import io.kinference.runners.AccuracyRunner
 import io.kinference.utils.TestRunner
 import kotlin.test.Test
 import kotlin.time.ExperimentalTime
@@ -21,7 +22,7 @@ class GPTRTest {
 
     @Test
     fun heavy_test_gpt_quantized_model() = TestRunner.runTest {
-        KIAccuracyRunner.runFromS3("gpt2:r-completion:quantized:v1", delta = 2.4)
+        KIAccuracyRunner.runFromS3("gpt2:r-completion:quantized:v1", delta = AccuracyRunner.QUANT_DELTA)
     }
 
     @Test
