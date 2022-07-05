f0f8a92bd352a07fa243a26431313a8a4860bba7
Ilya Vologin <ilya.vologin@jetbrains.com>
2022-07-05 16:07:05 +0300

    Added finalizer for ORT CPU kmath adapter

--- adapters/kmath/adapter-kmath-ort/src/jvmMain/kotlin/io/kinference/kmath/ORTKMathAdapter.kt
+++ adapters/kmath/adapter-kmath-ort/src/jvmMain/kotlin/io/kinference/kmath/ORTKMathAdapter.kt
@@ -2,6 +2,9 @@ package io.kinference.kmath
 
 import io.kinference.data.*
 import io.kinference.ort.ORTData
+import io.kinference.ort.data.map.ORTMap
+import io.kinference.ort.data.seq.ORTSequence
+import io.kinference.ort.data.tensor.ORTTensor
 import io.kinference.ort.model.ORTModel
 
 class ORTKMathAdapter(model: ORTModel) : ONNXModelAdapter<ORTKMathData<*>, ORTData<*>>(model) {
@@ -10,4 +13,14 @@ class ORTKMathAdapter(model: ORTModel) : ONNXModelAdapter<ORTKMathData<*>, ORTDa
         ONNXDataType.ONNX_SEQUENCE to ORTKMathSequenceAdapter,
         ONNXDataType.ONNX_MAP to ORTKMathMapAdapter
     ) as Map<ONNXDataType, ONNXDataAdapter<ORTKMathData<*>, ORTData<*>>>
+
+    override fun finalizeData(data: Collection<ORTData<*>>) {
+        for (element in data) {
+            when (element.type) {
+                ONNXDataType.ONNX_TENSOR -> (element as ORTTensor).data.close()
+                ONNXDataType.ONNX_SEQUENCE-> (element as ORTSequence).data.close()
+                ONNXDataType.ONNX_MAP -> (element as ORTMap).data.close()
+            }
+        }
+    }
 }
