7f11d4085348ff3b6f3eba2bec3b3fbcbcf0ceca
Ilya Vologin <ilya.vologin@jetbrains.com>
2022-07-11 16:57:16 +0300

    Update version to 0.1.19 for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 group = "io.kinference"
-version = "0.1.18"
+version = "0.1.19"
 
 plugins {
     kotlin("multiplatform") apply false
