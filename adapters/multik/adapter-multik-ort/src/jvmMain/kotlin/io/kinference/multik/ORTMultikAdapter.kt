94fd9ef148571248c878f32140efaf1a6f6f4fb9
Ilya Vologin <ilya.vologin@jetbrains.com>
2022-07-11 13:28:23 +0300

    Added finalize data for ORT Multik adapter

--- adapters/multik/adapter-multik-ort/src/jvmMain/kotlin/io/kinference/multik/ORTMultikAdapter.kt
+++ adapters/multik/adapter-multik-ort/src/jvmMain/kotlin/io/kinference/multik/ORTMultikAdapter.kt
@@ -2,6 +2,9 @@ package io.kinference.multik
 
 import io.kinference.data.*
 import io.kinference.ort.ORTData
+import io.kinference.ort.data.map.ORTMap
+import io.kinference.ort.data.seq.ORTSequence
+import io.kinference.ort.data.tensor.ORTTensor
 import io.kinference.ort.model.ORTModel
 
 class ORTMultikAdapter(model: ORTModel) : ONNXModelAdapter<ORTMultikData<*>, ORTData<*>>(model) {
@@ -10,4 +13,14 @@ class ORTMultikAdapter(model: ORTModel) : ONNXModelAdapter<ORTMultikData<*>, ORT
         ONNXDataType.ONNX_SEQUENCE to ORTMultikSequenceAdapter,
         ONNXDataType.ONNX_MAP to ORTMultikMapAdapter
     ) as Map<ONNXDataType, ONNXDataAdapter<ORTMultikData<*>, ORTData<*>>>
+
+    override fun finalizeData(data: Collection<ORTData<*>>) {
+        for (element in data) {
+            when(element.type) {
+                ONNXDataType.ONNX_TENSOR -> (element as ORTTensor).data.close()
+                ONNXDataType.ONNX_SEQUENCE-> (element as ORTSequence).data.close()
+                ONNXDataType.ONNX_MAP -> (element as ORTMap).data.close()
+            }
+        }
+    }
 }
