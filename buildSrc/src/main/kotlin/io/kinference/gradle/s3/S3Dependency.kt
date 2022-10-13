dcdd72d98c7b02517203c749a14080c430b1edb8
Anastasia Tuchina <anastasia.tuchina@jetbrains.com>
2022-10-14 01:38:56 +0200

    Add new FLCC model tests

--- buildSrc/src/main/kotlin/io/kinference/gradle/s3/S3Dependency.kt
+++ buildSrc/src/main/kotlin/io/kinference/gradle/s3/S3Dependency.kt
@@ -14,6 +14,8 @@ class S3Dependency(project: Project, private val path: String) {
                 s3Test("bert:standard:en:v1")
                 s3Test("bert:gec:en:standard:v2")
                 s3Test("gpt2:flcc-py-completion:quantized:v2")
+                s3Test("gpt2:flcc-py-completion:quantized:v3")
+                s3Test("gpt2:flcc-py-completion:standard:v3")
                 s3Test("gpt2:grazie:distilled:quantized:v6")
                 s3Test("gpt2:r-completion:standard:v1")
                 s3Test("gpt2:r-completion:quantized:v1")
--- inference/inference-core/src/commonTest/kotlin/io/kinference/models/gpt/GPTFLCCTest.kt
+++ inference/inference-core/src/commonTest/kotlin/io/kinference/models/gpt/GPTFLCCTest.kt
@@ -9,12 +9,32 @@ import kotlin.time.ExperimentalTime
 @ExperimentalTime
 class GPTFLCCTest {
     //    @Test
-    fun heavy_test_gpt_py_model() = TestRunner.runTest {
+    fun heavy_test_gpt_py_model_v2() = TestRunner.runTest {
         KIAccuracyRunner.runFromS3("gpt2:flcc-py-completion:quantized:v2", delta = 10.0)
     }
 
     @Test
-    fun benchmark_test_gpt_py_performance() = TestRunner.runTest {
-        KIPerformanceRunner.runFromS3("gpt2:flcc-py-completion:quantized:v2", count = 50)
+    fun heavy_test_gpt_py_model_v3() = TestRunner.runTest {
+        KIAccuracyRunner.runFromS3("gpt2:flcc-py-completion:standard:v3")
+    }
+
+    @Test
+    fun heavy_test_gpt_py_model_quantized_v3() = TestRunner.runTest {
+        KIAccuracyRunner.runFromS3("gpt2:flcc-py-completion:quantized:v3", delta = 1.5)
+    }
+
+    @Test
+    fun benchmark_test_gpt_py_model_v3() = TestRunner.runTest {
+        KIPerformanceRunner.runFromS3("gpt2:flcc-py-completion:standard:v3", count = 20)
+    }
+
+    @Test
+    fun benchmark_test_gpt_py_model_quantized_v3() = TestRunner.runTest {
+        KIPerformanceRunner.runFromS3("gpt2:flcc-py-completion:quantized:v3", count = 20)
+    }
+
+    @Test
+    fun benchmark_test_gpt_py_performance_v2() = TestRunner.runTest {
+        KIPerformanceRunner.runFromS3("gpt2:flcc-py-completion:quantized:v2", count = 20)
     }
 }
