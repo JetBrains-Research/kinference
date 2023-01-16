631b1a7147f52047a3a91b3a540adffa7d3b0aff
AnastasiaTuchina <anastasia.tuchina@jetbrains.com>
2023-01-16 19:35:32 +0100

    [release] Update version to 0.2.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 group = "io.kinference"
-version = "0.2.4"
+version = "0.2.5"
 
 plugins {
     kotlin("multiplatform") apply false
