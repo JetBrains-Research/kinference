01424efb387ecc5943aa4c7d5f756395b2f8ed5d
AnastasiaTuchina <anastasia.tuchina@jetbrains.com>
2023-03-24 16:46:11 +0100

    [Release] Update KInference version to 0.2.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 group = "io.kinference"
-version = "0.2.11"
+version = "0.2.12"
 
 plugins {
     kotlin("multiplatform") apply false
