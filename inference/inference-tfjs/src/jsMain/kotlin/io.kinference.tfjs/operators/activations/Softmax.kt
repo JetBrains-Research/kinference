ef6b4cfe02c478835b947e033bc668a78e21ee11
AnastasiaTuchina <anastasia.tuchina@jetbrains.com>
2023-05-03 12:40:11 +0200

    KI-9 [tfjs] Support Softmax for all available axes

--- inference/inference-tfjs/src/jsMain/kotlin/io.kinference.tfjs/operators/activations/Softmax.kt
+++ inference/inference-tfjs/src/jsMain/kotlin/io.kinference.tfjs/operators/activations/Softmax.kt
@@ -3,13 +3,11 @@ package io.kinference.tfjs.operators.activations
 import io.kinference.attribute.Attribute
 import io.kinference.data.ONNXData
 import io.kinference.graph.Contexts
-import io.kinference.ndarray.arrays.NumberNDArrayTFJS
-import io.kinference.ndarray.arrays.indexAxis
-import io.kinference.ndarray.extensions.tidyNDArrays
+import io.kinference.ndarray.arrays.*
+import io.kinference.ndarray.extensions.tidyNDArray
 import io.kinference.operator.*
 import io.kinference.protobuf.message.AttributeProto
-import io.kinference.tfjs.data.tensors.TFJSTensor
-import io.kinference.tfjs.data.tensors.asNamedOutputs
+import io.kinference.tfjs.data.tensors.*
 
 sealed class Softmax(
     name: String,
@@ -32,7 +30,12 @@ sealed class Softmax(
 
 //only for float and double types
 
-class SoftmaxVer1(name: String, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) : Softmax(name, INFO, attributes, inputs, outputs) {
+class SoftmaxVer1(
+    name: String,
+    attributes: Map<String, Attribute<Any>>,
+    inputs: List<String>,
+    outputs: List<String>
+) : Softmax(name, INFO, attributes, inputs, outputs) {
     companion object {
         private val TYPE_CONSTRAINTS = FLOAT_DATA_TYPES
 
@@ -45,15 +48,31 @@ class SoftmaxVer1(name: String, attributes: Map<String, Attribute<Any>>, inputs:
 
         internal val VERSION = VersionInfo(sinceVersion = 1, untilVersion = 13)
         private val INFO = OperatorInfo("Softmax", ATTRIBUTES_INFO, INPUT_INFO, OUTPUT_INFO, VERSION, OperatorInfo.DEFAULT_DOMAIN)
+
+        private suspend fun NumberNDArrayTFJS.softmaxNonLastAxis(axis: Int): NumberNDArrayTFJS {
+            return tidyNDArray {
+                val rows = this.computeBlockSize(toDim = axis)
+                val columns = this.computeBlockSize(fromDim = axis)
+                val matrixShape = intArrayOf(rows,columns)
+
+                val matrixLike = this.reshape(matrixShape).softmax(axis = -1)
+                matrixLike.reshape(this.shape)
+            }
+        }
     }
 
     private val axis: Int by attribute { it: Number -> it.toInt() }
 
     override suspend fun <D : ONNXData<*, *>> apply(contexts: Contexts<D>, inputs: List<TFJSTensor?>): List<TFJSTensor?> {
-        return tidyNDArrays {
-            val input = inputs[0]!!.data as NumberNDArrayTFJS
-            val actualAxis = input.indexAxis(axis)
-            arrayOf(input.softmax(actualAxis))
-        }.asNamedOutputs(outputs)
+        val input = inputs[0]!!.data as NumberNDArrayTFJS
+        val actualAxis = input.indexAxis(axis)
+
+        val result = if (actualAxis == input.shape.lastIndex) {
+            input.softmax(actualAxis)
+        } else {
+            input.softmaxNonLastAxis(actualAxis)
+        }
+
+        return listOf(result.asTensor("output"))
     }
 }
