8dace7899200c65f006550a961ab8b6488aa4917
Ilya Vologin <ilya.vologin@jetbrains.com>
2023-07-06 14:00:51 +0200

    KI-29 [core] Add IsNaN operation

---
+++ ndarray/ndarray-core/src/commonMain/kotlin/io/kinference/ndarray/extensions/isNaN/PrimitiveIsNaN.kt
@@ -0,0 +1,29 @@
+@file:GeneratePrimitives(
+    DataType.DOUBLE,
+    DataType.FLOAT
+)
+
+package io.kinference.ndarray.extensions.isNaN
+
+import io.kinference.ndarray.arrays.BooleanNDArray
+import io.kinference.ndarray.arrays.PrimitiveNDArray
+import io.kinference.primitives.annotations.GeneratePrimitives
+import io.kinference.primitives.types.DataType
+
+fun PrimitiveNDArray.isNaN(): BooleanNDArray {
+    val output = BooleanNDArray(strides)
+
+    val inputBlockIter = this.array.blocks.iterator()
+    val outputBlockIter = output.array.blocks.iterator()
+
+    for (blockIdx in 0 until this.array.blocksNum) {
+        val inputBlock = inputBlockIter.next()
+        val outputBlock = outputBlockIter.next()
+
+        for (idx in outputBlock.indices) {
+            outputBlock[idx] = inputBlock[idx].isNaN()
+        }
+    }
+
+    return output
+}
