75d19760a7adaaa4d655cc83e0c669b9edc0019f
AnastasiaTuchina <anastasia.tuchina@jetbrains.com>
2023-07-14 19:57:28 +0200

    KI-39 [core] Add Sqrt operator

--- inference/inference-core/src/commonMain/kotlin/io/kinference.core/operators/tensor/IsNaN.kt
+++ inference/inference-core/src/commonMain/kotlin/io/kinference.core/operators/tensor/IsNaN.kt
@@ -6,7 +6,7 @@ import io.kinference.core.data.tensor.asTensor
 import io.kinference.data.ONNXData
 import io.kinference.graph.Contexts
 import io.kinference.ndarray.arrays.*
-import io.kinference.ndarray.extensions.isNaN.isNaN
+import io.kinference.ndarray.extensions.isNaN
 import io.kinference.operator.*
 import io.kinference.primitives.types.DataType
 
---
+++ ndarray/ndarray-core/src/commonMain/kotlin/io/kinference/ndarray/extensions/PrimitiveElementWise.kt
@@ -0,0 +1,46 @@
+@file:GeneratePrimitives(DataType.ALL)
+
+package io.kinference.ndarray.extensions
+
+import io.kinference.ndarray.arrays.*
+import io.kinference.primitives.annotations.GeneratePrimitives
+import io.kinference.primitives.types.DataType
+import io.kinference.primitives.types.PrimitiveType
+
+fun PrimitiveNDArray.applyElementWise(func: (PrimitiveType) -> PrimitiveType): MutablePrimitiveNDArray {
+    val output = MutablePrimitiveNDArray(strides)
+
+    val inputBlockIter = array.blocks.iterator()
+    val outputBlockIter = output.array.blocks.iterator()
+    val blockSize = output.array.blockSize
+
+    repeat(output.array.blocksNum) {
+        val inputBlock = inputBlockIter.next()
+        val outputBlock = outputBlockIter.next()
+
+        for (idx in 0 until blockSize) {
+            outputBlock[idx] = func(inputBlock[idx])
+        }
+    }
+
+    return output
+}
+
+fun PrimitiveNDArray.predicateElementWise(predicate: (PrimitiveType) -> Boolean): BooleanNDArray {
+    val output = MutableBooleanNDArray(strides)
+
+    val inputBlockIter = array.blocks.iterator()
+    val outputBlockIter = output.array.blocks.iterator()
+    val blockSize = output.array.blockSize
+
+    repeat(output.array.blocksNum) {
+        val inputBlock = inputBlockIter.next()
+        val outputBlock = outputBlockIter.next()
+
+        for (idx in 0 until blockSize) {
+            outputBlock[idx] = predicate(inputBlock[idx])
+        }
+    }
+
+    return output
+}
