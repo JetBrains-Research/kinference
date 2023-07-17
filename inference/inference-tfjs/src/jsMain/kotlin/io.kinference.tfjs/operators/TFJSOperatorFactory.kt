f4de147ca21bf05ec9554333acd3d2e9782bb7e3
AnastasiaTuchina <anastasia.tuchina@jetbrains.com>
2023-07-17 16:28:28 +0200

    KI-43 [core, tfjs] Add Sin operator

--- inference/inference-tfjs/src/jsMain/kotlin/io.kinference.tfjs/operators/TFJSOperatorFactory.kt
+++ inference/inference-tfjs/src/jsMain/kotlin/io.kinference.tfjs/operators/TFJSOperatorFactory.kt
@@ -109,6 +109,7 @@ object TFJSOperatorFactory : OperatorFactory<TFJSData<*>> {
         "ScatterND" -> ScatterND(name, version, attributes, inputs, outputs)
         "Shape" -> Shape(name, version, attributes, inputs, outputs)
         "Sigmoid" -> Sigmoid(name, version, attributes, inputs, outputs)
+        "Sin" -> Sin(name, version, attributes, inputs, outputs)
         "Size" -> Size(name, version, attributes, inputs, outputs)
         "SkipLayerNormalization" -> SkipLayerNormalization(name, version, attributes, inputs, outputs)
         "Slice" -> Slice(name, version, attributes, inputs, outputs)
