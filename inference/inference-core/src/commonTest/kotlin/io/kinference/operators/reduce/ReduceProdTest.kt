e98cca6f1d9654998140c97c6fe162b4919e9b2f
Ilya Vologin <ilya.vologin@jetbrains.com>
2023-08-02 14:00:38 +0200

    KI-58 [core, tfjs] ReduceProd implementation and fixes for JS tests in core implementation

--- inference/inference-core/src/commonTest/kotlin/io/kinference/operators/reduce/ReduceProdTest.kt
+++ inference/inference-core/src/commonTest/kotlin/io/kinference/operators/reduce/ReduceProdTest.kt
@@ -1,6 +1,7 @@
 package io.kinference.operators.reduce
 
 import io.kinference.KITestEngine.KIAccuracyRunner
+import io.kinference.utils.Platform
 import io.kinference.utils.TestRunner
 import kotlin.test.Test
 
@@ -14,8 +15,17 @@ class ReduceProdVer18Test {
     }
 
     @Test
-    fun test_reduce_prod_default_axes_keepdims_random()  = TestRunner.runTest {
+    fun test_reduce_prod_default_axes_keepdims_random_jvm()  = TestRunner.runTest(Platform.JVM) {
         KIAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_default_axes_keepdims_random"))
+    }
+
+    /*
+     * This test for JS requires delta = 4e-3 because JS doesn't have float32 type
+     * and calculates everything with float64 consequently contains big error
+     */
+    @Test
+    fun test_reduce_prod_default_axes_keepdims_random_js()  = TestRunner.runTest(Platform.JS) {
+        KIAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_default_axes_keepdims_random"), delta = 4e-3)
     }
 
     @Test
@@ -58,8 +68,17 @@ class ReduceProdVer1Test {
     }
 
     @Test
-    fun test_reduce_prod_default_axes_keepdims_random()  = TestRunner.runTest {
+    fun test_reduce_prod_default_axes_keepdims_random_jvm()  = TestRunner.runTest(Platform.JVM) {
         KIAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_default_axes_keepdims_random"))
+    }
+
+    /*
+     * This test for JS requires delta = 4e-3 because JS doesn't have float32 type
+     * and calculates everything with float64 consequently contains big error
+     */
+    @Test
+    fun test_reduce_prod_default_axes_keepdims_random_js()  = TestRunner.runTest(Platform.JS) {
+        KIAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_default_axes_keepdims_random"), delta = 4e-3)
     }
 
     @Test
---
+++ inference/inference-tfjs/src/jsTest/kotlin/io/kinference/tfjs/operators/reduce/ReduceProdTest.kt
@@ -0,0 +1,102 @@
+package io.kinference.tfjs.operators.reduce
+
+import io.kinference.tfjs.runners.TFJSTestEngine.TFJSAccuracyRunner
+import io.kinference.utils.TestRunner
+import kotlin.test.Test
+
+
+class ReduceProdVer18Test {
+    private fun getTargetPath(dirName: String) = "reduce_prod/v18/$dirName/"
+
+    @Test
+    fun test_reduce_prod_default_axes_keepdims_example()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_default_axes_keepdims_example"))
+    }
+
+    /*
+     * This test for JS requires delta = 4e-3 because JS doesn't have float32 type
+     * and calculates everything with float64 consequently contains big error
+     */
+    @Test
+    fun test_reduce_prod_default_axes_keepdims_random()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_default_axes_keepdims_random"), delta = 2e-3)
+    }
+
+    @Test
+    fun test_reduce_prod_do_not_keepdims_example()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_do_not_keepdims_example"))
+    }
+
+    @Test
+    fun test_reduce_prod_do_not_keepdims_random()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_do_not_keepdims_random"))
+    }
+
+    @Test
+    fun test_reduce_prod_keepdims_example()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_keepdims_example"))
+    }
+
+    @Test
+    fun test_reduce_prod_keepdims_random()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_keepdims_random"))
+    }
+
+    @Test
+    fun test_reduce_prod_negative_axes_keepdims_example()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_negative_axes_keepdims_example"))
+    }
+
+    @Test
+    fun test_reduce_prod_negative_axes_keepdims_random()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_negative_axes_keepdims_random"))
+    }
+}
+
+class ReduceProdVer1Test {
+    private fun getTargetPath(dirName: String) = "reduce_prod/v1/$dirName/"
+
+    @Test
+    fun test_reduce_prod_default_axes_keepdims_example()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_default_axes_keepdims_example"))
+    }
+
+    /*
+     * This test for JS requires delta = 4e-3 because JS doesn't have float32 type
+     * and calculates everything with float64 consequently contains big error
+     */
+    @Test
+    fun test_reduce_prod_default_axes_keepdims_random()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_default_axes_keepdims_random"), delta = 2e-3)
+    }
+
+    @Test
+    fun test_reduce_prod_do_not_keepdims_example()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_do_not_keepdims_example"))
+    }
+
+    @Test
+    fun test_reduce_prod_do_not_keepdims_random()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_do_not_keepdims_random"))
+    }
+
+    @Test
+    fun test_reduce_prod_keepdims_example()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_keepdims_example"))
+    }
+
+    @Test
+    fun test_reduce_prod_keepdims_random()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_keepdims_random"))
+    }
+
+    @Test
+    fun test_reduce_prod_negative_axes_keepdims_example()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_negative_axes_keepdims_example"))
+    }
+
+    @Test
+    fun test_reduce_prod_negative_axes_keepdims_random()  = TestRunner.runTest {
+        TFJSAccuracyRunner.runFromResources(getTargetPath("test_reduce_prod_negative_axes_keepdims_random"))
+    }
+}
