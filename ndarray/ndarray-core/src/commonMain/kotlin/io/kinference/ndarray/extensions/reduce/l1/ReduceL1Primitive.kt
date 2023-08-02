09d20db59fd24ca5e7fb7783da775a86f3757af7
Ilya Vologin <ilya.vologin@jetbrains.com>
2023-08-02 15:55:43 +0200

    KI-61 [core] ReduceL1 operator implementation

---
+++ ndarray/ndarray-core/src/commonMain/kotlin/io/kinference/ndarray/extensions/reduce/l1/ReduceL1Primitive.kt
@@ -0,0 +1,23 @@
+@file:GeneratePrimitives(
+    DataType.FLOAT,
+    DataType.DOUBLE,
+    DataType.INT,
+    DataType.LONG,
+    DataType.UINT,
+    DataType.ULONG,
+)
+package io.kinference.ndarray.extensions.reduce.l1
+
+import io.kinference.ndarray.arrays.PrimitiveNDArray
+import io.kinference.ndarray.extensions.reduce.primitive.reduceOperationPrimitive
+import io.kinference.primitives.annotations.GeneratePrimitives
+import io.kinference.primitives.types.DataType
+import io.kinference.primitives.types.PrimitiveType
+import io.kinference.primitives.annotations.MakePublic
+import io.kinference.ndarray.stubs.abs
+import io.kinference.ndarray.extensions.abs
+import kotlin.math.abs
+
+@MakePublic
+internal fun PrimitiveNDArray.reduceL1(axes: IntArray, keepDims: Boolean) =
+    reduceOperationPrimitive(axes, keepDims) { out: PrimitiveType, inp: PrimitiveType -> out + abs(inp) }
