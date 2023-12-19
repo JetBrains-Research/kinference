6157a9a294b76581a599aae8c93d7e0eff6ac21c
Ilya Vologin <ilya.vologin@jetbrains.com>
2023-12-19 15:13:09 +0100

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 group = "io.kinference"
-version = "0.2.17-kotlin18"
+version = "0.2.17-kotlin18-1"
 
 plugins {
     kotlin("multiplatform") apply false
