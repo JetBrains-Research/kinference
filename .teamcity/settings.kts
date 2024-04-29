7730799736a656410247cf2be5ba12e0190f3228
Julia.Reshetnikova <julia.reshetnikova@jetbrains.com>
2024-04-29 16:53:22 +0200

    remove aws values

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.ParameterDisplay.HIDDEN
-import jetbrains.buildServer.configs.kotlin.buildFeatures.provideAwsCredentials
 import jetbrains.buildServer.configs.kotlin.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.projectFeatures.awsConnection
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 version = "2024.03"
@@ -34,11 +32,6 @@ project {
         password("env.JB_SPACE_CLIENT_SECRET", "", display = HIDDEN)
     }
 
-    fun ParametrizedWithType.publisher() {
-        password("env.PUBLISHER_ID", "", display = HIDDEN)
-        password("env.PUBLISHER_KEY", "", display = HIDDEN)
-    }
-
     fun BuildFeatures.defaultSwabra() {
         swabra {
             verbose = true
@@ -48,33 +41,13 @@ project {
 
     vcsRoot(kiPrimitivesVcsRoot)
 
-    features {
-        awsConnection {
-            id = "AWS_Grazie_Dev_KInference"
-            name = "Amazon Web Services (AWS)"
-            regionName = "eu-west-1"
-            credentialsType = static {
-                accessKeyId = "AKIA453I4BURLGEJ5UNX"
-                secretAccessKey = "credentialsJSON:7b5b7eb6-e9f0-4788-ad3f-ad0f0a1ead7a"
-                stsEndpoint = "https://sts.eu-west-1.amazonaws.com"
-            }
-            allowInSubProjects = true
-            allowInBuilds = true
-        }
-    }
-
     buildType {
         name = "Release"
         id = RelativeId(name.toId())
         vcs { root(kiVcsRoot) }
         steps { release("--parallel --console=plain --no-daemon") }
-        params {
-            spacePackagesAccessRW()
-        }
-        features {
-            provideAwsCredentials { this.awsConnectionId = "AWS_Grazie_Dev_KInference" }
-            defaultSwabra()
-        }
+        params { spacePackagesAccessRW() }
+        features { defaultSwabra() }
     }
 
     buildType {
@@ -82,13 +55,7 @@ project {
         id = RelativeId(name.toId())
         vcs { root(kiPrimitivesVcsRoot) }
         steps { release() }
-        params {
-            publisher()
-            spacePackagesAccessRW()
-        }
-        features {
-            provideAwsCredentials { this.awsConnectionId = "AWS_Grazie_Dev_KInference" }
-            defaultSwabra()
-        }
+        params { spacePackagesAccessRW() }
+        features { defaultSwabra() }
     }
 }
