f496fc4a30c0f8a82420c66bd63a6b8103c5bb22
dmitriyb <dvbozhko@gmail.com>
2024-07-25 18:37:39 +0200

    JBAI-4832 [ndarray, utils] Removed ConcurrentQueue implementation for commonMain and move to jvm specific ConcurrentLinkedQueue without expect/actual.

--- ndarray/ndarray-core/src/jvmMain/kotlin/io/kinference/ndarray/arrays/memory/ModelArrayStorage.kt
+++ ndarray/ndarray-core/src/jvmMain/kotlin/io/kinference/ndarray/arrays/memory/ModelArrayStorage.kt
@@ -2,11 +2,10 @@ package io.kinference.ndarray.arrays.memory
 
 import io.kinference.ndarray.arrays.ArrayTypes
 import io.kinference.utils.Closeable
-
-import io.kinference.utils.ConcurrentQueue
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class ModelArrayStorage(private val limiter: MemoryLimiter = MemoryLimiters.Default) : Closeable {
-    private val unusedArrays: ConcurrentQueue<ArrayStorage> = ConcurrentQueue()
+    private val unusedArrays: ConcurrentLinkedQueue<ArrayStorage> = ConcurrentLinkedQueue()
 
     companion object {
         private const val INIT_SIZE_VALUE: Int = 2
@@ -26,10 +25,10 @@ class ModelArrayStorage(private val limiter: MemoryLimiter = MemoryLimiters.Defa
     }
 
     private fun getStorage(): ArrayStorage {
-        return unusedArrays.removeFirstOrNull() ?: ArrayStorage(typeSize, INIT_SIZE_VALUE)
+        return unusedArrays.poll() ?: ArrayStorage(typeSize, INIT_SIZE_VALUE)
     }
 
     private fun returnStorage(storage: ArrayStorage) {
-        unusedArrays.addLast(storage)
+        unusedArrays.offer(storage)
     }
 }
