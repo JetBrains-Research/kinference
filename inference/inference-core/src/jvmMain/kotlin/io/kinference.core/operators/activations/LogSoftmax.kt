e94f68c912ee540e465775b5d35e82eac000ba98
Tommaso Dossi <doxeno.2003@gmail.com>
2025-04-01 17:28:21 +0200

    re-added softmax v1

--- inference/inference-core/src/jvmMain/kotlin/io/kinference.core/operators/activations/LogSoftmax.kt
+++ inference/inference-core/src/jvmMain/kotlin/io/kinference.core/operators/activations/LogSoftmax.kt
@@ -10,9 +10,10 @@ import io.kinference.protobuf.message.AttributeProto
 
 sealed class LogSoftmax(name: String, info: OperatorInfo, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) : Activation(name, info, attributes, inputs, outputs) {
     companion object {
-        private val DEFAULT_VERSION = VersionInfo(sinceVersion = 13, untilVersion = 23)
+        private val DEFAULT_VERSION = VersionInfo(sinceVersion = 13);
 
         operator fun invoke(name: String, version: Int?, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) = when (version ?: DEFAULT_VERSION.sinceVersion) {
+            in LogSoftmaxVer13.VERSION.asRange() -> LogSoftmaxVer13(name, attributes, inputs, outputs)
             in LogSoftmaxVer1.VERSION.asRange() -> LogSoftmaxVer1(name, attributes, inputs, outputs)
             else -> error("Unsupported version of LogSoftmax operator: $version")
         }
@@ -26,9 +27,30 @@ class LogSoftmaxVer1(name: String, attributes: Map<String, Attribute<Any>>, inpu
         private val INPUT_INFO = listOf(IOInfo(0, TYPE_CONSTRAINTS, "input", optional = false))
         private val OUTPUT_INFO = listOf(IOInfo(0, TYPE_CONSTRAINTS, "output", optional = false))
 
+        private val ATTRIBUTES_INFO = listOf(AttributeInfo("axis", setOf(AttributeProto.AttributeType.INT), false, default = 1))
+
+        internal val VERSION = VersionInfo(sinceVersion = 1, untilVersion = 13)
+        private val INFO = OperatorInfo("LogSoftmax", ATTRIBUTES_INFO, INPUT_INFO, OUTPUT_INFO, VERSION, OperatorInfo.DEFAULT_DOMAIN)
+    }
+
+    val axis: Int by attribute { it: Number -> it.toInt() }
+
+    override suspend fun activate(input: NDArrayCore, contexts: Contexts<KIONNXData<*>>): NDArrayCore {
+        input as NumberNDArrayCore
+        return input.logSoftmaxVer1(axis)
+    }
+}
+
+class LogSoftmaxVer13(name: String, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) : LogSoftmax(name, INFO, attributes, inputs, outputs) {
+    companion object {
+        private val TYPE_CONSTRAINTS = FLOAT_DATA_TYPES
+
+        private val INPUT_INFO = listOf(IOInfo(0, TYPE_CONSTRAINTS, "input", optional = false))
+        private val OUTPUT_INFO = listOf(IOInfo(0, TYPE_CONSTRAINTS, "output", optional = false))
+
         private val ATTRIBUTES_INFO = listOf(AttributeInfo("axis", setOf(AttributeProto.AttributeType.INT), false, default = -1))
 
-        internal val VERSION = VersionInfo(sinceVersion = 13, untilVersion = 23)
+        internal val VERSION = VersionInfo(sinceVersion = 13)
         private val INFO = OperatorInfo("LogSoftmax", ATTRIBUTES_INFO, INPUT_INFO, OUTPUT_INFO, VERSION, OperatorInfo.DEFAULT_DOMAIN)
     }
 
--- inference/inference-core/src/jvmMain/kotlin/io/kinference.core/operators/activations/Softmax.kt
+++ inference/inference-core/src/jvmMain/kotlin/io/kinference.core/operators/activations/Softmax.kt
@@ -10,9 +10,10 @@ import io.kinference.protobuf.message.AttributeProto
 
 sealed class Softmax(name: String, info: OperatorInfo, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) : Activation(name, info, attributes, inputs, outputs) {
     companion object {
-        private val DEFAULT_VERSION = VersionInfo(sinceVersion = 13, untilVersion = 23)
+        private val DEFAULT_VERSION = VersionInfo(sinceVersion = 1)
 
         operator fun invoke(name: String, version: Int?, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) = when (version ?: DEFAULT_VERSION.sinceVersion) {
+            in SoftmaxVer13.VERSION.asRange() -> SoftmaxVer13(name, attributes, inputs, outputs)
             in SoftmaxVer1.VERSION.asRange() -> SoftmaxVer1(name, attributes, inputs, outputs)
             else -> error("Unsupported version of Softmax operator: $version")
         }
@@ -26,13 +27,36 @@ class SoftmaxVer1(name: String, attributes: Map<String, Attribute<Any>>, inputs:
         private val TYPE_CONSTRAINTS = FLOAT_DATA_TYPES
 
         private val ATTRIBUTES_INFO = listOf(
+            AttributeInfo("axis", setOf(AttributeProto.AttributeType.INT), false, default = 1)
+        )
+
+        private val INPUT_INFO = listOf(IOInfo(0, TYPE_CONSTRAINTS, "input", optional = false))
+        private val OUTPUT_INFO = listOf(IOInfo(0, TYPE_CONSTRAINTS, "output", optional = false))
+
+        internal val VERSION = VersionInfo(sinceVersion = 1, untilVersion = 13)
+        private val INFO = OperatorInfo("Softmax", ATTRIBUTES_INFO, INPUT_INFO, OUTPUT_INFO, VERSION, OperatorInfo.DEFAULT_DOMAIN)
+    }
+
+    private val axis: Int by attribute { it: Number -> it.toInt() }
+
+    override suspend fun activate(input: NDArrayCore, contexts: Contexts<KIONNXData<*>>): NDArrayCore {
+        input as NumberNDArrayCore
+        return input.softmaxVer1(axis)
+    }
+}
+
+class SoftmaxVer13(name: String, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) : Softmax(name, INFO, attributes, inputs, outputs) {
+    companion object {
+        private val TYPE_CONSTRAINTS = FLOAT_DATA_TYPES
+
+        private val ATTRIBUTES_INFO = listOf(
             AttributeInfo("axis", setOf(AttributeProto.AttributeType.INT), false, default = -1)
         )
 
         private val INPUT_INFO = listOf(IOInfo(0, TYPE_CONSTRAINTS, "input", optional = false))
         private val OUTPUT_INFO = listOf(IOInfo(0, TYPE_CONSTRAINTS, "output", optional = false))
 
-        internal val VERSION = VersionInfo(sinceVersion = 13, untilVersion = 23)
+        internal val VERSION = VersionInfo(sinceVersion = 13)
         private val INFO = OperatorInfo("Softmax", ATTRIBUTES_INFO, INPUT_INFO, OUTPUT_INFO, VERSION, OperatorInfo.DEFAULT_DOMAIN)
     }
 
