a555c51f89a8de32d0cdf8649b5bf30c6e2590d7
Tommaso Dossi <doxeno.2003@gmail.com>
2025-04-28 14:19:58 +0200

    Changed default softmax version

--- inference/inference-core/src/jvmMain/kotlin/io/kinference.core/operators/activations/Softmax.kt
+++ inference/inference-core/src/jvmMain/kotlin/io/kinference.core/operators/activations/Softmax.kt
@@ -10,7 +10,7 @@ import io.kinference.protobuf.message.AttributeProto
 
 sealed class Softmax(name: String, info: OperatorInfo, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) : Activation(name, info, attributes, inputs, outputs) {
     companion object {
-        private val DEFAULT_VERSION = VersionInfo(sinceVersion = 1)
+        private val DEFAULT_VERSION = VersionInfo(sinceVersion = 13)
 
         operator fun invoke(name: String, version: Int?, attributes: Map<String, Attribute<Any>>, inputs: List<String>, outputs: List<String>) = when (version ?: DEFAULT_VERSION.sinceVersion) {
             in SoftmaxVer13.VERSION.asRange() -> SoftmaxVer13(name, attributes, inputs, outputs)
