7fcc915ba5e718676e20be73cf7d46e35b06f142
Tommaso Dossi <doxeno.2003@gmail.com>
2025-05-02 14:38:59 +0200

    Updated Softmax to version 13

--- ndarray/ndarray-core/src/jvmMain/kotlin/io/kinference/ndarray/extensions/softmax/SoftmaxPrimitive.kt
+++ ndarray/ndarray-core/src/jvmMain/kotlin/io/kinference/ndarray/extensions/softmax/SoftmaxPrimitive.kt
@@ -10,14 +10,117 @@ import io.kinference.primitives.annotations.GenerateNameFromPrimitives
 import io.kinference.primitives.annotations.GeneratePrimitives
 import io.kinference.primitives.types.*
 import io.kinference.ndarray.math.FastMath
-import io.kinference.ndarray.math.exp
 import io.kinference.ndarray.stubs.MIN_VALUE_FOR_MAX
 import kotlin.math.*
 import io.kinference.ndarray.extensions.*
+import io.kinference.ndarray.math.exp
+
 
 
 @GenerateNameFromPrimitives
-internal suspend fun softmaxPrimitive(input: PrimitiveNDArray, dest: MutablePrimitiveNDArray, rows: Int, columns: Int): MutablePrimitiveNDArray {
+internal suspend fun softmaxVer13Primitive(input: PrimitiveNDArray, dest: MutablePrimitiveNDArray, rows: Int, columns: Int, stride: Int): MutablePrimitiveNDArray {
+    val inputBlockSize = input.array.blockSize
+    val inputBlocks = input.array.blocks
+    val outputArray = dest.array
+    val maxesArray = PrimitiveTiledArray(rows * stride, inputBlockSize)
+    maxesArray.fill(PrimitiveType.MIN_VALUE_FOR_MAX)
+
+    val blocksInStride = stride/inputBlockSize
+    val stridesInRow = columns / stride
+
+    //Finding Max along the axis
+    parallelizeByRows(columns, rows, 1048576) { rowStart, rowEnd, _ ->
+        for (rowNum in rowStart until rowEnd) {
+            val rowStrideStart = rowNum * stridesInRow
+            val maxBlockStart = rowNum * blocksInStride
+
+            for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
+                val strideStartBlock = strideNum * blocksInStride
+
+                for (blockNum in 0 until blocksInStride) {
+                    val inputBlock = inputBlocks[strideStartBlock + blockNum]
+                    val maxBlock = maxesArray.blocks[maxBlockStart + blockNum]
+
+                    for (j in  inputBlock.indices) {
+                        maxBlock[j] = max(inputBlock[j], maxBlock[j])
+                    }
+                }
+            }
+
+            for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
+                val strideStartBlock = strideNum * blocksInStride
+
+                for (blockNum in 0 until blocksInStride) {
+                    val maxBlock = maxesArray.blocks[maxBlockStart + blockNum]
+                    val inputBlock = inputBlocks[strideStartBlock + blockNum]
+                    val outputBlock = outputArray.blocks[strideStartBlock + blockNum]
+
+                    for (j in  inputBlock.indices) {
+                        outputBlock[j] = inputBlock[j] - maxBlock[j]
+                    }
+                }
+            }
+
+        }
+    }
+
+    // Apply exp for output array
+    parallelizeByBlocks(inputBlockSize, inputBlocks.size, 2048) { blockStart, blockEnd, _ ->
+        for (blockNum in blockStart until blockEnd) {
+            val outputBlock = outputArray.blocks[blockNum]
+
+            for (j in outputBlock.indices) {
+                outputBlock[j] = FastMath.exp(outputBlock[j])
+            }
+        }
+    }
+
+    val sumsArray = PrimitiveTiledArray(rows*stride, inputBlockSize)
+    sumsArray.fill((0).toPrimitive())
+
+    // Compute sums along axis and divide output by them
+    parallelizeByRows(columns, rows, 1048576) { rowStart, rowEnd, _ ->
+        for (rowNum in rowStart until rowEnd) {
+            val rowStrideStart = rowNum * stridesInRow
+            val sumBlockStart = rowNum * blocksInStride
+
+            for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
+                val strideStartBlock = strideNum * blocksInStride
+
+                for (blockNum in 0 until blocksInStride) {
+                    val outputBlock = outputArray.blocks[strideStartBlock + blockNum]
+                    val sumBlock = sumsArray.blocks[sumBlockStart + blockNum]
+
+                    for (j in outputBlock.indices) {
+                        sumBlock[j] += outputBlock[j]
+                    }
+                }
+            }
+
+            for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
+                val strideStartBlock = strideNum * blocksInStride
+
+                for (blockNum in 0 until blocksInStride) {
+                    val outputBlock = outputArray.blocks[strideStartBlock + blockNum]
+                    val sumBlock = sumsArray.blocks[sumBlockStart + blockNum]
+
+                    for (j in outputBlock.indices) {
+                        outputBlock[j] /= sumBlock[j]
+                    }
+                }
+            }
+        }
+    }
+
+    return dest
+}
+
+@GenerateNameFromPrimitives
+internal suspend fun softmaxVer13Primitive(input: PrimitiveNDArray, rows: Int, columns: Int, stride: Int): MutablePrimitiveNDArray =
+    softmaxVer13Primitive(input, MutablePrimitiveNDArray(PrimitiveTiledArray(input.linearSize, input.array.blockSize), input.strides), rows, columns, stride)
+
+@GenerateNameFromPrimitives
+internal suspend fun softmaxVer1Primitive(input: PrimitiveNDArray, dest: MutablePrimitiveNDArray, rows: Int, columns: Int): MutablePrimitiveNDArray {
     val inputBlockSize = input.array.blockSize
     val inputBlocks = input.array.blocks
 
@@ -111,5 +214,5 @@ internal suspend fun softmaxPrimitive(input: PrimitiveNDArray, dest: MutablePrim
 }
 
 @GenerateNameFromPrimitives
-internal suspend fun softmaxPrimitive(input: PrimitiveNDArray, rows: Int, columns: Int): MutablePrimitiveNDArray =
-    softmaxPrimitive(input, MutablePrimitiveNDArray(PrimitiveTiledArray(input.linearSize, input.array.blockSize), input.strides), rows, columns)
+internal suspend fun softmaxVer1Primitive(input: PrimitiveNDArray, rows: Int, columns: Int): MutablePrimitiveNDArray =
+    softmaxVer1Primitive(input, MutablePrimitiveNDArray(PrimitiveTiledArray(input.linearSize, input.array.blockSize), input.strides), rows, columns)
