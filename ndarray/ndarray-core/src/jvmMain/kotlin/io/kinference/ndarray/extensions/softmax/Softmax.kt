e1e15fc1f6c05e27068d33731e3c4c0ab32d2a84
Tomaso Dossi <doxeno.2003@gmail.com>
2025-07-11 11:50:30 +0200

    minor refactor

--- ndarray/ndarray-core/src/jvmMain/kotlin/io/kinference/ndarray/extensions/softmax/Softmax.kt
+++ ndarray/ndarray-core/src/jvmMain/kotlin/io/kinference/ndarray/extensions/softmax/Softmax.kt
@@ -8,22 +8,16 @@ suspend fun softmax(
     axis: Int = -1,
 ): MutableNumberNDArrayCore {
     require(input.type == DataType.FLOAT || input.type == DataType.DOUBLE)
-        { "Softmax operation supported only for FLOAT and DOUBLE tensors, actual type is ${input.type}" }
+    { "Softmax operation supported only for FLOAT and DOUBLE tensors, actual type is ${input.type}" }
 
     val actualAxis = input.indexAxis(axis)
     val rows = input.computeBlockSize(toDim = actualAxis)
     val columns = input.computeBlockSize(fromDim = actualAxis)
-    val stride = if(actualAxis == input.rank - 1) 1 else input.computeBlockSize(fromDim = actualAxis + 1)
+    val stride = if (actualAxis == input.rank - 1) 1 else input.computeBlockSize(fromDim = actualAxis + 1)
 
-    return when(input.type) {
-        DataType.FLOAT -> when(stride) {
-            1 -> softmaxVer1Float(input as FloatNDArray, rows, columns)
-            else -> softmaxVer13Float(input as FloatNDArray, rows, columns, stride)
-        }
-        DataType.DOUBLE -> when(stride) {
-            1 -> softmaxVer1Double(input as DoubleNDArray, rows, columns)
-            else -> softmaxVer13Double(input as DoubleNDArray, rows, columns, stride)
-        }
+    return when (input.type) {
+        DataType.FLOAT -> softmaxVer13Float(input as FloatNDArray, rows, columns, stride)
+        DataType.DOUBLE -> softmaxVer13Double(input as DoubleNDArray, rows, columns, stride)
         else -> error("Softmax operation supported only for FLOAT and DOUBLE tensors, actual type is ${input.type}")
     }
 }
@@ -40,17 +34,19 @@ suspend fun softmax(
     val actualAxis = input.indexAxis(axis)
     val rows = input.computeBlockSize(toDim = actualAxis)
     val columns = input.computeBlockSize(fromDim = actualAxis)
-    val stride = if(actualAxis == input.rank - 1) 1 else input.computeBlockSize(fromDim = actualAxis + 1)
+    val stride = if (actualAxis == input.rank - 1) 1 else input.computeBlockSize(fromDim = actualAxis + 1)
 
-    return when(input.type) {
-        DataType.FLOAT -> when(stride) {
+    return when (input.type) {
+        DataType.FLOAT -> when (stride) {
             1 -> softmaxVer1Float(input as FloatNDArray, dest as MutableFloatNDArray, rows, columns)
             else -> softmaxVer13Float(input as FloatNDArray, dest as MutableFloatNDArray, rows, columns, stride)
         }
-        DataType.DOUBLE -> when(stride) {
+
+        DataType.DOUBLE -> when (stride) {
             1 -> softmaxVer1Double(input as DoubleNDArray, dest as MutableDoubleNDArray, rows, columns)
             else -> softmaxVer13Double(input as DoubleNDArray, dest as MutableDoubleNDArray, rows, columns, stride)
         }
+
         else -> error("Softmax operation supported only for FLOAT and DOUBLE tensors, actual type is ${input.type}")
     }
 }
@@ -60,15 +56,15 @@ suspend fun softmaxVer1(
     axis: Int = 0,
 ): MutableNumberNDArrayCore {
     require(input.type == DataType.FLOAT || input.type == DataType.DOUBLE)
-        { "Softmax operation supported only for FLOAT and DOUBLE tensors, actual type is ${input.type}" }
+    { "Softmax operation supported only for FLOAT and DOUBLE tensors, actual type is ${input.type}" }
 
     val actualAxis = input.indexAxis(axis)
     val rows = input.computeBlockSize(toDim = actualAxis)
     val columns = input.computeBlockSize(fromDim = actualAxis)
 
-    return when(input.type) {
+    return when (input.type) {
         DataType.FLOAT -> softmaxVer1Float(input as FloatNDArray, rows, columns)
-        DataType.DOUBLE ->  softmaxVer1Double(input as DoubleNDArray, rows, columns)
+        DataType.DOUBLE -> softmaxVer1Double(input as DoubleNDArray, rows, columns)
         else -> error("Softmax operation supported only for FLOAT and DOUBLE tensors, actual type is ${input.type}")
     }
 }
@@ -86,9 +82,9 @@ suspend fun softmaxVer1(
     val rows = input.computeBlockSize(toDim = actualAxis)
     val columns = input.computeBlockSize(fromDim = actualAxis)
 
-    return when(input.type) {
+    return when (input.type) {
         DataType.FLOAT -> softmaxVer1Float(input as FloatNDArray, dest as MutableFloatNDArray, rows, columns)
-        DataType.DOUBLE ->  softmaxVer1Double(input as DoubleNDArray, dest as MutableDoubleNDArray, rows, columns)
+        DataType.DOUBLE -> softmaxVer1Double(input as DoubleNDArray, dest as MutableDoubleNDArray, rows, columns)
         else -> error("Softmax operation supported only for FLOAT and DOUBLE tensors, actual type is ${input.type}")
     }
 }
--- ndarray/ndarray-core/src/jvmMain/kotlin/io/kinference/ndarray/extensions/softmax/SoftmaxPrimitive.kt
+++ ndarray/ndarray-core/src/jvmMain/kotlin/io/kinference/ndarray/extensions/softmax/SoftmaxPrimitive.kt
@@ -16,58 +16,87 @@ import io.kinference.ndarray.extensions.*
 import io.kinference.ndarray.math.exp
 
 
-
 @GenerateNameFromPrimitives
-internal suspend fun softmaxVer13Primitive(input: PrimitiveNDArray, dest: MutablePrimitiveNDArray, rows: Int, columns: Int, stride: Int): MutablePrimitiveNDArray {
+internal suspend fun softmaxVer13Primitive(
+    input: PrimitiveNDArray,
+    dest: MutablePrimitiveNDArray,
+    rows: Int,
+    columns: Int,
+    stride: Int
+): MutablePrimitiveNDArray {
     val inputBlockSize = input.array.blockSize
     val inputBlocks = input.array.blocks
     val outputArray = dest.array
-    val maxesArray = PrimitiveTiledArray(rows * stride, inputBlockSize)
-    maxesArray.fill(PrimitiveType.MIN_VALUE_FOR_MAX)
+    val outputBlocks = outputArray.blocks
 
-    val blocksInStride = stride/inputBlockSize
+    val blocksInStride = stride / inputBlockSize
+    val blocksInRow = columns / inputBlockSize
     val stridesInRow = columns / stride
 
     //Finding Max along the axis
-    parallelizeByRows(columns, rows, 1048576) { rowStart, rowEnd, _ ->
-        for (rowNum in rowStart until rowEnd) {
-            val rowStrideStart = rowNum * stridesInRow
-            val maxBlockStart = rowNum * blocksInStride
+    if (stride > 1) {
+        val maxesArray = PrimitiveTiledArray(rows * stride, inputBlockSize)
+        maxesArray.fill(PrimitiveType.MIN_VALUE_FOR_MAX)
+        parallelizeByRows(columns, rows, 1048576) { rowStart, rowEnd, _ ->
+            for (rowNum in rowStart until rowEnd) {
+                val rowStrideStart = rowNum * stridesInRow
+                val maxBlockStart = rowNum * blocksInStride
 
-            for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
-                val strideStartBlock = strideNum * blocksInStride
+                for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
+                    val strideStartBlock = strideNum * blocksInStride
 
-                for (blockNum in 0 until blocksInStride) {
-                    val inputBlock = inputBlocks[strideStartBlock + blockNum]
-                    val maxBlock = maxesArray.blocks[maxBlockStart + blockNum]
+                    for (blockNum in 0 until blocksInStride) {
+                        val inputBlock = inputBlocks[strideStartBlock + blockNum]
+                        val maxBlock = maxesArray.blocks[maxBlockStart + blockNum]
 
-                    for (j in  inputBlock.indices) {
+                        for (j in inputBlock.indices) {
+                            maxBlock[j] = max(inputBlock[j], maxBlock[j])
+                        }
+                    }
+                }
+
+                for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
+                    val strideStartBlock = strideNum * blocksInStride
+
+                    for (blockNum in 0 until blocksInStride) {
+                        val maxBlock = maxesArray.blocks[maxBlockStart + blockNum]
+                        val inputBlock = inputBlocks[strideStartBlock + blockNum]
+                        val outputBlock = outputBlocks[strideStartBlock + blockNum]
+
+                        for (j in inputBlock.indices) {
+                            outputBlock[j] = inputBlock[j] - maxBlock[j]
+                        }
+                    }
+                }
+
+            }
+        }
+    } else {
+        parallelizeByRows(columns, rows, 1048576) { rowStart, rowEnd, _ ->
+            for (row in rowStart until rowEnd) {
+                val maxBlock = PrimitiveArray(inputBlockSize) { PrimitiveType.MIN_VALUE_FOR_MAX }
+                for (blockIdx in row * blocksInRow until (row + 1) * blocksInRow) {
+                    val inputBlock = inputBlocks[blockIdx]
+                    for (j in inputBlock.indices) {
                         maxBlock[j] = max(inputBlock[j], maxBlock[j])
                     }
                 }
-            }
-
-            for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
-                val strideStartBlock = strideNum * blocksInStride
-
-                for (blockNum in 0 until blocksInStride) {
-                    val maxBlock = maxesArray.blocks[maxBlockStart + blockNum]
-                    val inputBlock = inputBlocks[strideStartBlock + blockNum]
-                    val outputBlock = outputArray.blocks[strideStartBlock + blockNum]
-
-                    for (j in  inputBlock.indices) {
+                maxBlock.fill(maxBlock.max())
+                for (blockIdx in row * blocksInRow until (row + 1) * blocksInRow) {
+                    val inputBlock = inputBlocks[blockIdx]
+                    val outputBlock = outputBlocks[blockIdx]
+                    for (j in inputBlock.indices) {
                         outputBlock[j] = inputBlock[j] - maxBlock[j]
                     }
                 }
             }
-
         }
     }
 
     // Apply exp for output array
     parallelizeByBlocks(inputBlockSize, inputBlocks.size, 2048) { blockStart, blockEnd, _ ->
         for (blockNum in blockStart until blockEnd) {
-            val outputBlock = outputArray.blocks[blockNum]
+            val outputBlock = outputBlocks[blockNum]
 
             for (j in outputBlock.indices) {
                 outputBlock[j] = FastMath.exp(outputBlock[j])
@@ -75,41 +104,65 @@ internal suspend fun softmaxVer13Primitive(input: PrimitiveNDArray, dest: Mutabl
         }
     }
 
-    val sumsArray = PrimitiveTiledArray(rows*stride, inputBlockSize)
-    sumsArray.fill((0).toPrimitive())
+    if (stride > 1) {
+        val sumsArray = PrimitiveTiledArray(rows * stride, inputBlockSize)
+        val sumBlocks = sumsArray.blocks
+        sumsArray.fill((0).toPrimitive())
 
-    // Compute sums along axis and divide output by them
-    parallelizeByRows(columns, rows, 1048576) { rowStart, rowEnd, _ ->
-        for (rowNum in rowStart until rowEnd) {
-            val rowStrideStart = rowNum * stridesInRow
-            val sumBlockStart = rowNum * blocksInStride
+        parallelizeByRows(columns, rows, 1048576) { rowStart, rowEnd, _ ->
+            for (rowNum in rowStart until rowEnd) {
+                val rowStrideStart = rowNum * stridesInRow
+                val sumBlockStart = rowNum * blocksInStride
 
-            for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
-                val strideStartBlock = strideNum * blocksInStride
+                for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
+                    val strideStartBlock = strideNum * blocksInStride
 
-                for (blockNum in 0 until blocksInStride) {
-                    val outputBlock = outputArray.blocks[strideStartBlock + blockNum]
-                    val sumBlock = sumsArray.blocks[sumBlockStart + blockNum]
+                    for (blockNum in 0 until blocksInStride) {
+                        val outputBlock = outputBlocks[strideStartBlock + blockNum]
+                        val sumBlock = sumBlocks[sumBlockStart + blockNum]
 
-                    for (j in outputBlock.indices) {
-                        sumBlock[j] += outputBlock[j]
+                        for (j in outputBlock.indices) {
+                            sumBlock[j] += outputBlock[j]
+                        }
                     }
                 }
-            }
 
-            for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
-                val strideStartBlock = strideNum * blocksInStride
+                for (strideNum in rowStrideStart until rowStrideStart + stridesInRow) {
+                    val strideStartBlock = strideNum * blocksInStride
 
-                for (blockNum in 0 until blocksInStride) {
-                    val outputBlock = outputArray.blocks[strideStartBlock + blockNum]
-                    val sumBlock = sumsArray.blocks[sumBlockStart + blockNum]
+                    for (blockNum in 0 until blocksInStride) {
+                        val outputBlock = outputBlocks[strideStartBlock + blockNum]
+                        val sumBlock = sumBlocks[sumBlockStart + blockNum]
 
-                    for (j in outputBlock.indices) {
-                        outputBlock[j] /= sumBlock[j]
+                        for (j in outputBlock.indices) {
+                            outputBlock[j] /= sumBlock[j]
+                        }
                     }
                 }
             }
         }
+    } else {
+        parallelizeByRows(columns, rows, 1048576) { rowStart, rowEnd, _ ->
+            val sumsArray = PrimitiveArray(inputBlockSize)
+            for (row in rowStart until rowEnd) {
+                sumsArray.fill((0).toPrimitive())
+                for (blockIdx in row * blocksInRow until (row + 1) * blocksInRow) {
+                    val outputBlock = outputBlocks[blockIdx]
+                    for (j in outputBlock.indices) {
+                        sumsArray[j] += outputBlock[j]
+                    }
+                }
+
+                sumsArray.fill(sumsArray.sum())
+                for (blockIdx in row * blocksInRow until (row + 1) * blocksInRow) {
+                    val outputBlock = outputBlocks[blockIdx]
+                    for (j in outputBlock.indices) {
+                        outputBlock[j] /= sumsArray[j]
+                    }
+                }
+            }
+        }
+
     }
 
     return dest
