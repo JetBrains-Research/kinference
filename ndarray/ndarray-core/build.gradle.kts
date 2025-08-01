701fc0893d6086c896cdecf5dcfdc92fe6ae264c
Tommaso Dossi <doxeno.2003@gmail.com>
2025-08-01 11:35:47 +0200

    bugfix

--- ndarray/ndarray-core/build.gradle.kts
+++ ndarray/ndarray-core/build.gradle.kts
@@ -31,7 +31,7 @@ benchmark {
         }
 
         var types = arrayOf("Double", "Float")
-        val benchmarks = arrayOf("Dot", "Elu", "Softmax", "Logistic", "Neg", "Probit", "Gelu")
+        val benchmarks = arrayOf("Dot", "Elu", "Exp", "Gelu", "Logistic", "Neg", "Probit", "Softmax")
         for (type in types)
             register(type.lowercase()) {
                 include(".*$type.*")
--- ndarray/ndarray-core/src/jvmBenchmark/kotlin/io/kinference/ndarray/Utils.kt
+++
@@ -1,7 +0,0 @@
-package io.kinference.ndarray
-
-fun randomFloat(l: Float = -1024f, r: Float = 1024f) =
-    (r - l) * Math.random().toFloat() + l
-
-fun randomDouble(l: Double = -1024.0, r: Double = 1024.0) =
-    (r - l) * Math.random() + l
